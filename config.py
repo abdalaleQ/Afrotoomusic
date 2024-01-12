e287b584aa33092f18b2c6e54101a2f978e553cf
abdalaleQ <153198040+abdalaleQ@users.noreply.github.com>
2024-01-12 06:07:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/e74815820b06773515851.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/e74815820b06773515851.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/T_Y_E_X")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/T_Y_E_X")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/rr_r_v")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/rr_r_v")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5904216848").split()))
 
