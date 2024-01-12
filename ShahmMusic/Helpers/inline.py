37ca49a6890f26d0d6c7eee9f6aae6bfd9e1f5da
abdalaleQ <153198040+abdalaleQ@users.noreply.github.com>
2024-01-12 06:10:26 +0300

    Update inline.py

--- ShahmMusic/Helpers/inline.py
+++ ShahmMusic/Helpers/inline.py
@@ -37,7 +37,7 @@ pm_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="", url="https://t.me/SH_AH_M"
+            text="", url="https://t.me/rr_r_v"
         ),
         InlineKeyboardButton(text="◈ مالك البوت ◈", user_id=config.OWNER_ID),
     ],
@@ -57,7 +57,7 @@ gp_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="◈ المطور ◈", url="https://t.me/IIUll_l"
+            text="◈ المطور ◈", url="https://t.me/T_5_G"
         ),
         InlineKeyboardButton(text="◈ مالك البوت ◈", user_id=config.OWNER_ID),
     ],
